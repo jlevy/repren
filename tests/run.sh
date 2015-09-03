c608015ce730e38832e0a6c7b622c6489e7f2757
Joshua Levy <joshua@cal.berkeley.edu>
2015-09-03 00:11:18 -0700

    Add tests.
    Good start at #1.
    Still could use a lot more.

---
+++ tests/run.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Primitive but effective test harness for running command-line regression tests.
+# This is all rather ugly but once this harness works, you only need to look at the
+# tests file and occasionally edit the cleanup patterns below.
+
+set -euo pipefail
+trap "echo && echo 'Tests failed! See failure above.'" ERR
+
+dir="$(cd `dirname $0`; pwd)"
+
+full_log=${1:-$dir/tests-full.log}
+clean_log=${2:-$dir/tests-clean.log}
+
+
+echo Cleaning up...
+
+rm -rf "$dir/tmp-dir"
+cp -a $dir/work-dir $dir/tmp-dir
+cd $dir/tmp-dir
+
+echo "Running..."
+
+# Hackity hack:
+# Remove per-run and per-platform details to allow easy comparison.
+# Update these patterns as appropriate.
+# Note we use perl not sed, so it works on Mac and Linux.
+# The $|=1; is just for the impatient and ensures line buffering.
+# We also use the cat trick below so it's possible to view the full log as it
+# runs on stderr while writing to both logs.
+$dir/tests.sh 2>&1 \
+  | tee $full_log \
+  | tee >(cat 1>&2) \
+  | perl -pe '$|=1; s/([a-zA-Z0-9._]+.py):[0-9]+/\1:xx/g' \
+  | perl -pe '$|=1; s/File ".*\/([a-zA-Z0-9._]+.py)", line [0-9]*,/File "...\/\1", line __X,/g' \
+  | perl -pe '$|=1; s/, line [0-9]*,/, line __X,/g' \
+  | perl -pe '$|=1; s/partial.[a-z0-9]*/partial.__X/g' \
+  | perl -pe '$|=1; s/ at 0x[0-9a-f]*/ at 0x__X/g' \
+  | perl -pe '$|=1; s/[0-9.:T-]*Z/__TIMESTAMP/g' \
+  | perl -pe '$|=1; s|/private/tmp/|/tmp/|g' \
+  > $clean_log
+
+echo "Tests done."
+echo
+echo "Full log: $full_log"
+echo "Clean log: $clean_log"
+echo
+echo "Validation is manual. To compare regression test results with previously correct output, run:"
+echo "git diff $clean_log"
---
+++ tests/tests.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+# Test script. Output of this script can be saved and compared to test for regressions.
+# Double-spacing between commands here makes the script output easier to read.
+
+# We turn on exit on error, so that any status code changes cause a test failure.
+set -e -o pipefail
+
+prog_name=repren
+base_dir=`dirname $0`
+prog=$base_dir/../repren
+
+args=
+#args=--debug
+
+run() {
+  $prog $args "$@"
+}
+
+# A trick to test for error conditions.
+expect_error() {
+  echo "(got expected error: status $?)"
+}
+
+# A trick to do ls portably, showing just permissions and file types.
+ls_portable() {
+  ls -lF "$@" | tail +2 | awk '{print $1, $NF}'
+}
+
+# This will echo all commands as they are read. Bash commands plus their
+# outputs will be used for validating regression tests pass (set -x is similar
+# but less readable and sometimes not deterministic).
+set -v
+
+# --- Start of tests ---
+
+# Platform and Python version we're using to run tests.
+uname
+
+python -V
+
+repren || expect_error
+
+# Text replacements, no renames.
+
+cp -a original test1
+
+run -n --from Humpty --to Dumpty test1/humpty-dumpty.txt
+
+diff -r original test1
+
+run --from humpty --to dumpty test1/humpty-dumpty.txt
+
+diff original test1
+
+run --from Humpty --to Dumpty test1/humpty-dumpty.txt
+
+diff -r original test1 || expect_error
+
+run --from humpty --to dumpty test1
+
+
+# File renames only.
+
+cp -a original test2
+
+run -n --renames --from humpty --to dumpty test2
+
+ls_portable test2
+
+run --renames --from humpty --to dumpty test2
+
+ls_portable test2
+
+diff -r original test2 || expect_error
+
+
+# Both file renames and replacements.
+
+cp -a original test3
+
+run -n --full -i --from humpty --to dumpty test3
+
+ls_portable test3
+
+run --full -i --from humpty --to dumpty test3
+
+ls_portable test3
+
+diff -r original test3 || expect_error
+
+
+# More patterns: Contents.
+
+cp -a original test4
+
+run -p patterns-misc test4
+
+diff -r original test4 || expect_error
+
+
+# More patterns: Contents and renames.
+
+cp -a original test5
+
+run --full -i -p patterns-misc test5
+
+diff -r original test5 || expect_error
+
+
+# Preserving case.
+
+cp -a original test6
+
+run --full --preserve-case -p patterns-rotate-abc test6/humpty-dumpty.txt
+
+diff -r original test6 || expect_error
+
+
+# A few rotations to get back to where we started.
+
+cp -a original test7
+
+run --full --preserve-case -p patterns-rotate-abc test7
+
+run --full --preserve-case -p patterns-rotate-abc test7
+
+run --full --preserve-case -p patterns-rotate-abc test7
+
+find test7 -name \*.orig -delete
+
+diff -r original test7
+
+
+# Whole-word mode.
+
+cp -a original test8
+
+run --full --word-breaks -p patterns-rotate-abc test8
+
+diff -r original/humpty-dumpty.txt test8/humpty-dumpty.txt || expect_error
+
+
+# Moving files.
+
+# TODO: Fix this.
+# cp -a original test9
+# run --renames --from stuff/trees --to another-dir test9
+
+
+# TODO: More test coverage:
+# - Regex and capturing groups.
+# - CamelCase and whole word support.
+# - Large stress test (rename a variable in a large source package and recompile).
+
+# Leave files installed in case it's helpful to debug anything.
+
+# --- End of tests ---
